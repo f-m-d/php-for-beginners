d31f00f610d02f3117c077be9105fef67bfa16b9
Francesco Maria D'Auria <dauriafrancescomaria@gmail.com>
2023-02-06 23:08:06 +0100

    PHP basic tags + variables in code

---
+++ Section1/comment.php
@@ -0,0 +1,14 @@
+<?php
+
+// This function display the "Hello comment" string!!
+echo "Hello comment!";
+
+/* This is
+a
+multiline
+comment!!! */
+echo "Hello comment multiline!!"
+
+
+
+?>
---
+++ Section1/dynamic.php
@@ -0,0 +1,9 @@
+<?php
+
+$name = "Frank Main Page";
+
+?>
+
+
+<!-- #Remember: you can declare a PHP statement and HTML code calling PHP!!-->
+<h1> <?php echo $name; ?></h1>
